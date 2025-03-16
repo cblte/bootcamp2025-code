8ce83220bf2115d314d7c24d1e78b760507ba068
Carsten Brueggenolte <mail@cbrueggenolte.de>
2025-03-16 18:30:03 +0100

    Create e3-6.js

---
+++ e3-6.js
@@ -0,0 +1,8 @@
+/*
+Take the string "JavaScript is amazing" and generate a new string with the text "JavaScript", cutting the unneeded part
+*/
+
+// %%
+const text = "JavaScript is amazing";
+const newText = text.slice(0, 10);
+console.log(newText);
