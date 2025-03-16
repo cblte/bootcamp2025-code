fd697d171d9ac628bbc805b538821f7e95301e30
Carsten Brueggenolte <mail@cbrueggenolte.de>
2025-03-16 15:46:54 +0100

    Fix error messages and conditions in oven simulator

--- e3-3-oven-sim.js
+++ e3-3-oven-sim.js
@@ -1,8 +1,10 @@
-// Create a microwave oven "simulator" with an Oven class that has some state
-// variables to identify its current state provides the methods to inspect the
-// state, set the timer to start cooking, end cooking when the timer stops,
-// manually stop cooking by pressing a button, and open the oven and remove
-// the food.
+/**
+ * Create a microwave oven "simulator" with an Oven class that has some state
+ * variables to identify its current state provides the methods to inspect the
+ * state, set the timer to start cooking, end cooking when the timer stops,
+ * manually stop cooking by pressing a button, and open the oven and remove
+ * the food.
+ */
 
 class Oven {
   constructor() {
@@ -21,23 +23,23 @@ class Oven {
 
   putFood() {
     if (this.started) {
-      throw "Cannot put food inside, when oven is started.";
+      throw "Cannot put food inside, when oven is not stopped.";
     }
     if (this.doorOpen && !this.foodInside) {
       this.foodInside = true;
     } else {
-      throw "Cannot put food inside, when door is not open or food is already inside.";
+      throw "Cannot put food inside, when door is closed or food is inside.";
     }
   }
 
   removeFood() {
     if (this.started) {
-      throw "Cannot remove food, when oven is started.";
+      throw "Cannot remove food, when oven is not stopped.";
     }
-    if (this.started && this.doorOpen && this.foodInside) {
+    if (this.doorOpen && this.foodInside) {
       this.foodInside = false;
     } else {
-      throw "Cannot remove food, when door is not open or food is not inside.";
+      throw "Cannot remove food, when door is closed or no food is inside.";
     }
   }
 
@@ -52,6 +54,7 @@ class Oven {
   stop() {
     this.started = false;
   }
+
   /**
    * Starts the cooking process for a specified duration.
    * @param {number} duration - The cooking duration in seconds.
@@ -70,13 +73,3 @@ class Oven {
     }
   }
 }
-
-const oven = new Oven();
-
-oven.openDoor();
-console.log("Door Open? :" + oven.doorOpen);
-
-oven.putFood();
-oven.startCooking(5);
-console.log("Cooking started?: " + oven.started);
-oven.removeFood();
